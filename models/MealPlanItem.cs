c2a9b31608ad3c3c4f275e7488bb9c19cb63ee33
Theamazinguero <114643224+Theamazinguero@users.noreply.github.com>
2025-11-20 14:58:39 -0600

    Create MealPlanItem.cs

---
+++ models/MealPlanItem.cs
@@ -0,0 +1,25 @@
+// File: Models/MealPlanItem.cs
+namespace RecipeApp.Api.Models
+{
+    public enum MealType
+    {
+        Breakfast,
+        Lunch,
+        Dinner,
+        Snack
+    }
+
+    public class MealPlanItem
+    {
+        public int Id { get; set; }
+
+        public int MealPlanId { get; set; }
+        public MealPlan? MealPlan { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe? Recipe { get; set; }
+
+        public DateTime Date { get; set; }
+        public MealType MealType { get; set; }
+    }
+}
